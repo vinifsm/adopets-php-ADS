fe5f981f81795276ccaf63266e0f2b8012753af8
Xtreme44 <vini.fsm9@live.com>
2022-06-09 21:52:02 -0300

    Criação da Home

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    include 'menu.php';
+    include 'conexao.php';
+    $pdo = Conexao::conectar();
+    $sql  = "select * from animal order by nome;";
+    
+    $lstDog = $pdo->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <title>Listar Imovéis</title>
+</head>
+<div class="banner"></div>
+<body>
+    <?php
+foreach($lstDog as $adocao){
+    ?>
+<div class="blocks">
+<div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="<?php echo $adocao['imagem'] ?>">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4"><?php echo $adocao['nome'] ?><i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Adotar</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Descrição<i class="material-icons right">close</i></span>
+      <p><?php echo $adocao['descricao'] ?></p>
+    </div>
+  </div>
+</div>
+    <?php
+    }
+    ?>
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>Sistema Imobiliário</title>
+</head>
+
+<body>
+    <nav>
+        <div class="nav-wrapper teal darken-2">
+            <a href="index.php" class="brand-logo center"><img src="images\R.png" width="100"></a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down space">
+                <li><a href="lstImovel.php">Quero Adotar</a></li>
+                <li><a href="frmInsImovel.php">Quero Divulgar</a></li>
+            </ul>
+            <ul id="nav-mobile" class="right hide-on-med-and-down space1">
+            <li><a href="logout.php">Login</a></li>
+            <li><a href="logout.php">Cadastro</a></li>
+                </ul>
+        </div>
+    </nav>
+<ul id="slide-out" class="sidenav">
+  <li><div class="user-view">
+    <div class="background blue">
+    </div>
+    <a href="#user"></a>
+    <a href="#name"><span class="white-text name">Vinícius Moreira</span></a>
+    <a href="#email"><span class="white-text email">vini.fsm9@live.com</span></a>
+  </div></li>
+  <li><a class="waves-effect" href="https://www.fema.edu.br"><i class="material-icons">house</i>FEMA</a></li>
+  <li><a class="waves-effect" href="https://github.com/Xtreme44"><i class="material-icons">computer</i>GitHub</a></li>
+</ul>
+<a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      
+</body>
+
+</html>
+
+<script type="text/javascript">
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+</script>
